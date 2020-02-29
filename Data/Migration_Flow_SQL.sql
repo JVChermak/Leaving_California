3d30771b1899f4fea97acd467a5e321701b679d1
Angela Dominguez <aodoming154@gmail.com>
2020-02-29 01:59:42 -0800

    Adding Data and Query Folders with files

--- Data/Migration_Flow_SQL.sql
+++ Data/Migration_Flow_SQL.sql
@@ -1,15 +1,15 @@
 -- Creating tables for Migration-FlowDB
 
-CREATE TABLE State(
+CREATE TABLE States(
 	state_no int NOT NULL,
 	region_no int NOT NULL,
 	population_1YR_ago float NOT NULL,
 	different_state_1YR_ago float NOT NULL,
 	PRIMARY KEY(state_no),
-	UNIQUE(region_no)
+	-- UNIQUE(region_no)
 );
 DROP TABLE State CASCADE;
-SELECT * FROM State
+SELECT * FROM States
 
 CREATE TABLE Region(
 	region_no int NOT NULL,
@@ -27,6 +27,14 @@ CREATE TABLE Housing_and_Income(
 	median_home_value float NOT NULL,
 	median_RE_taxes float NOT NULL,
 	PRIMARY KEY(state_no),
-	FOREIGN KEY(state_no) REFERENCES State(state_no)
+	--FOREIGN KEY(state_no) REFERENCES State(state_no)
 );
 SELECT * FROM Housing_and_Income
+
+CREATE TABLE CA_Migration_2Other_States(
+	state_no int NOT NULL,
+	state_name varchar NOT NULL,
+	total_from_CA float NOT NULL,
+	PRIMARY KEY(state_no)
+); 
+SELECT * FROM CA_Migration_2Other_States
