d2afe334a720971a995bd7d750cdb8d81222536c
Jasmin Chermak <jasminchermak@Jasmins-MacBook-Air.local>
2020-03-17 23:06:51 -0700

    Adding table.

--- Dashboard/static/js/app.js
+++ Dashboard/static/js/app.js
@@ -19,13 +19,16 @@ $("path, circle").hover(function(e) {
     // change value of tooltip to html we just made
     $('#info-box').html(state_html);
   });
+
+  $(document).mousemove(function(e) {
+    $('#info-box').css('top',e.pageY-$('#info-box').height()-700);
+    $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
+  }).mouseover();
+
   $("path, circle").mouseleave(function(e) {
     $('#info-box').css('display','none');
   });
-  $(document).mousemove(function(e) {
-    $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
-    $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
-  }).mouseover();
+
   var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
   if(ios) {
     $('a').on('click touchend', function() {
