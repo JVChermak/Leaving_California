159c4e5fca064f3b72e86392a9fcf41c8b7b9d61
Jasmin Chermak <jasminchermak@Jasmins-MacBook-Air.local>
2020-03-20 00:16:58 -0700

    Adding dynamic table.

--- Dashboard/static/js/app.js
+++ Dashboard/static/js/app.js
@@ -1,44 +1,68 @@
-$("path, circle").hover(function(e) {
-    // make tooltip visible
-    $('#info-box').css('display','block');
-    // get year from selector element
-    const year = document.querySelector('#myList').value;
-    // filter the `data` array for states just in that year
-    const filtered = data.filter(d => d.Year == year);
-    // filter states of that year to just the one state matching the id of 
-    // the path that is being hovered on 
-    const state = filtered.filter(d => d.id == $(this).attr('id'))[0];
-    // create the html string to populate the tooltip with 
-    // as long as the key isn't 'id' then continue building
-    let state_html = '';
-    Object.entries(state).forEach(([key, value]) => {
-      if (key != 'id') {
-      state_html += `${key}: ${value}<br>`;
+// import the data from data.js
+const tableData = data;
+// Reference the HTML table using d3
+var tbody = d3.select("#migration-table");
+
+// Create a function to build the dynamic table
+function buildTable(data) {
+  // First, clear out any existing data
+  tbody.html("");
+  // Next, loop through each object in the data and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+    // Loop through each field in teh dataRow and add each value as a table cell
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
       }
-    })
-    // change value of tooltip to html we just made
-    $('#info-box').html(state_html);
+    );
   });
+}
 
-  $(document).mousemove(function(e) {
-    $('#info-box').css('top',e.pageY-$('#info-box').height()-700);
-    $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
-  }).mouseover();
 
-  $("path, circle").mouseleave(function(e) {
-    $('#info-box').css('display','none');
-  });
+// Keep track of all filters
+var filters = {};
 
-  var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-  if(ios) {
-    $('a').on('click touchend', function() {
-      var link = $(this).attr('href');
-      window.open(link,'_blank');
-      return false;
-    });
+// This function will replace your handleClick function
+function updateFilters() {
+
+  // Save the element, value, and id of the filter that was changed
+  let year = d3.select("#year").property("value");
+  let state = d3.select("#state").property("value");
+  
+  // If a filter value was entered then add that filterId and value
+  // to the filters list. Otherwise, clear that filter from the filters object
+  if (year) {
+    filters["Year"] = year;
+  }  else {
+      delete filters["Year"];
   }
-  function getOption() {
-    const selectElement = document.querySelector('#myList');
-    output = selectElement.value;
-    document.querySelector('.output').textContent = output;
+  if (state) {
+    filters["State"] = state;
+  }  else {
+       delete filters["State"];
   }
+  
+  // Call function to apply all filters and rebuild the table
+  filterTable(filters);
+}
+
+function filterTable(filters) {
+
+  // Set the filteredData to the tableData
+  let filteredData = tableData;
+  // Loop through all of the filters and keep any data that
+  // matches the filter values
+  Object.entries(filters).forEach(([key, value]) => {
+    filteredData = filteredData.filter(row => row[key] === value);
+  })
+  console.log(Object.entries(filters));
+  // Finally, rebuild the table using the filtered Data
+  buildTable(filteredData);
+}
+
+// Attach an event to listen for changes to each filter
+d3.selectAll("#filter-btn").on("click", updateFilters);
+// Build the table when the page loads
+buildTable(tableData);
